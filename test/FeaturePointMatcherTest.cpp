e038c75ba20e55f0da83749717ac36d684f0a3cb
Marcin Łapiński <lapinskm@gmail.com>
2014-04-29 21:45:21 +0200

    Added callbackIsCalled test of FeaturePointMatcher
    +Removed unnecessary code and comments
    +Fixed indentation

--- test/FeaturePointMatcherTest.cpp
+++ test/FeaturePointMatcherTest.cpp
@@ -5,12 +5,17 @@
 
 using namespace cv;
 
-
 // To use a test fixture, derive a class from testing::Test.
 class FeaturePointMatcherTest : public testing::Test
 {
  protected:  // You should make the members protected s.t. they can be
              // accessed from sub-classes.
+
+  //all setup work which can be don once.
+  FeaturePointMatcherTest()
+  {
+     initTestData();
+  }
 
   // virtual void SetUp() will be called before each test is run.  You
   // should define it if you need to initialize the varaibles.
@@ -31,9 +36,9 @@ class FeaturePointMatcherTest : public testing::Test
   virtual void TearDown()
   {
      cout<<"[ TearDown ]\n";
-     image.release();
      delete fpm;
      callbackCalled=false;
+     callbackFinished=false;
   }
 
   static void callback(void* result,void* userData)
@@ -46,45 +51,78 @@ class FeaturePointMatcherTest : public testing::Test
      owner->callbackFinished=true;
   }
 
+  void findDescriptors(const char* filename, Mat* descriptors)
+  {
+    Mat image;
+    Ptr<FeatureDetector>     detector=FeatureDetector::create("FAST");
+    Ptr<DescriptorExtractor> extractor=DescriptorExtractor::create("FREAK");
+    vector<KeyPoint> keypoints;
+
+    image = imread( filename, 1 );
+    detector  -> detect  (image, keypoints);
+    extractor -> compute (image, keypoints,*descriptors);
+  }
+
+  void initTestData()
+  {
+    findDescriptors("lena-gray.png"        , & lena_descr);
+    findDescriptors("lena-gray-full.png"   , & lena_full_descr);
+    findDescriptors("lena-gray-120deg.png" , & lena_120deg_descr);
+    findDescriptors("lena-gray-scalled.png", & lena_scalled_descr);
+    empty_descr=Mat::zeros(1, 1, CV_32F);
+  }
+
+
   // Declares the variables your tests want to use.
-  Mat image;
+
+  Mat empty_descr;
+  Mat lena_descr;
+  Mat lena_full_descr;
+  Mat lena_120deg_descr;
+  Mat lena_scalled_descr;
+
   FeaturePointMatcher* fpm;
   bool callbackCalled;
   bool callbackFinished;
   std::vector< DMatch >* cbResult;
-  //int foo;
-  //int bar;
 };
 
-// When you have a test fixture, you define a test using TEST_F
-// instead of TEST.
-
+/**************************TESTS SECTION***************************/
 // Tests the default c'tor.
 TEST_F(FeaturePointMatcherTest, defaultConstructor)
 {
-   EXPECT_TRUE(fpm);
+  EXPECT_TRUE(fpm);
 }
 
 TEST_F(FeaturePointMatcherTest, nullParameters)
 {
-   ASSERT_TRUE(fpm);
-   VVResultCode ret;
-   Mat descr=Mat::zeros(1, 1, CV_32F);
-   ret=fpm->startMatching(NULL, &descr, callback, this);
-   EXPECT_EQ(ret,vVWrongParams);
-   ret=fpm->startMatching(&descr, NULL, callback, this);
-   EXPECT_EQ(ret,vVWrongParams);
-   ret=fpm->startMatching(NULL, NULL, callback, this);
-   EXPECT_EQ(ret,vVWrongParams);
-   ret=fpm->startMatching(NULL, NULL, NULL, this);
-   EXPECT_EQ(ret,vVWrongParams);
-   ret=fpm->startMatching(&descr, &descr, NULL, this);
-   EXPECT_EQ(ret,vVWrongParams);
+  ASSERT_TRUE(fpm);
+  VVResultCode ret;
+  Mat descr=Mat::zeros(1, 1, CV_32F);
+  ret=fpm->startMatching(NULL, &empty_descr, callback, this);
+  EXPECT_EQ(ret,vVWrongParams);
+  ret=fpm->startMatching(&empty_descr, NULL, callback, this);
+  EXPECT_EQ(ret,vVWrongParams);
+  ret=fpm->startMatching(NULL, NULL, callback, this);
+  EXPECT_EQ(ret,vVWrongParams);
+  ret=fpm->startMatching(NULL, NULL, NULL, this);
+  EXPECT_EQ(ret,vVWrongParams);
+  ret=fpm->startMatching(&empty_descr, &empty_descr, NULL, this);
+  EXPECT_EQ(ret,vVWrongParams);
 }
 
 //test if callback is called
 TEST_F(FeaturePointMatcherTest, callbackIsCalled)
 {
+  ASSERT_TRUE(fpm);
+  VVResultCode ret;
+  ret=fpm->startMatching(&empty_descr, &empty_descr, callback, this);
+  EXPECT_EQ(ret,vVSuccess);
+  cout<<"[          ] Matcher launched\n";
+  //FIXME: use some timeout mechanism instead of simple waiting
+  cout<<"[          ] waiting 2 sec for finish matching\n";
+  sleep(2);
+  EXPECT_TRUE(callbackCalled);
 }
 
 //check if result is not NULL
