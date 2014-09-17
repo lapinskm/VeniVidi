447d6a300f2b632ab33f24e2a653aeab1ef6984c
Marcin Łapiński <lapinskm@gmail.com>
2014-09-17 22:03:10 +0200

    DataTypes.hpp file refactoring

--- include/DataTypes.hpp
+++ include/DataTypes.hpp
@@ -13,35 +13,42 @@ namespace VV
 class DataPoint3dVector
 {
   public:
-    std::shared_ptr<cv::Mat> getDescriptors() {return descriptors;}
-    std::vector<cv::Point3d> getPoints() {return points;}
-    std::vector<int> getImageIds(int i) {return imageIds[i];}
+
+    void addPoint(cv::Point3d point, cv::Mat descriptor);
+    void addImageToPoint(int imageId);
+
+    cv::Mat getDescriptors() {return m_descriptors;}
+    std::vector<cv::Point3d> getPoints() {return m_points;}
+    std::vector<int> getImageIds(int i) {return m_imageIds[i];}
 
   private:
     //descriptor for each data point
-    std::shared_ptr<cv::Mat> descriptors;
+    cv::Mat m_descriptors;
 
     /* Coordinates for each data point.
      * Values makes only sense if corresponding imageIds vector
      * is longer than 2, so triangulation was possible.*/
-    std::vector<cv::Point3d> points;
+    std::vector<cv::Point3d> m_points;
 
     //vector of ImageIds for each data point
-    std::vector<std::vector<int>> imageIds;
+    std::vector<std::vector<int>> m_imageIds;
 
 };
 
 class DataPoint2dVector
 {
   public:
-    //probably we won't need store descriptors here
-    //std::shared_ptr<cv:Mat> getDescriptors(){return descriptors;}
-
-    std::vector<cv::Point2d> getPoints() {return points;}
+    DataPoint2dVector(std::vector<cv::Point2d> points, cv::Mat descriptors)
+    :m_points(points)
+    ,m_descriptors(descriptors)
+    {}
+    cv::Mat getDescriptors(){return m_descriptors;}
+    std::vector<cv::Point2d> getPoints() {return m_points;}
 
   private:
-    //std::shared_ptr<cv:Mat>  descriptors;
-    std::vector<cv::Point2d> points;
+    DataPoint2dVector();
+    cv::Mat  m_descriptors;
+    std::vector<cv::Point2d> m_points;
 };
 
 /* Class containing data needed to be stored for each image
