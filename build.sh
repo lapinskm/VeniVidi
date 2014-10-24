529227d1c0cd6ff29a5e79965e03d4b8ba179c49
Marcin Łapiński <lapinskm@gmail.com>
2014-10-24 14:45:44 +0200

    Added Debug/Release switch to build.sh script.
    Added flags for each build type. Added debugging symbols for Debug, and
    optimization for Release.

--- build.sh
+++ build.sh
@@ -6,14 +6,27 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 # Default variable values:
 clean_build=0
 run_tests=0
-build_dir="./outDir"
+debug_build=0
+
+cmake_options=""
+
+#oneliner from internet
+repo_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+build_dir="$repo_dir/outDir/Release/"
 
 function show_help {
-echo "Help not implemeted"
-#TODO: display instruction
+echo "Usage: ./build [OPITONS]"
+echo ""
+echo "  -h,      displays help"
+echo "  -c,      clears build directory"
+echo "  -t,      launches tests afer succesfull build"
+echo "  -d,      builds as debug"
+echo ""
 }
 
-while getopts "h?ctd:" opt; do
+#Script options handling
+while getopts "h?ctd" opt; do
     case "$opt" in
     h|\?)
         show_help
@@ -23,7 +36,7 @@ while getopts "h?ctd:" opt; do
         ;;
     t)  run_tests=1
         ;;
-    d)  build_dir=$OPTARG
+    d)  debug_build=1
     esac
 done
 
@@ -31,22 +44,35 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
+#debug/release setup
+if [ $debug_build -eq 1 ]; then
+    build_dir="$repo_dir/outDir/Debug/"
+    cmake_options="-DCMAKE_BUILD_TYPE=Debug"
+else
+    build_dir="$repo_dir/outDir/Release/"
+    cmake_options="-DCMAKE_BUILD_TYPE=Release"
+fi
 
+#cleaning build folder
 if [ $clean_build -eq 1 ]; then
 
-    if [ -d ./build ]; then
+    if [ -d $build_dir ]; then
         rm -rf $build_dir 
     fi
 fi
 
+#actual build
 mkdir -p $build_dir
-cd $build_dir
+pushd $build_dir
 
-cmake ..
+cmake $repo_dir $cmake_options
+
 #TODO made number of used cores a setable option
-make -j2 
+time make -j2 
 
+#test launching
 if [ $run_tests -eq 1 ]; then
     ./runUnitTests
 fi
 
+popd
