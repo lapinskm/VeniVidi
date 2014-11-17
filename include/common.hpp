7e90a2342253d6b3fb42652713a1626266ea623a
Marcin Łapiński <lapinskm@gmail.com>
2014-11-17 22:41:42 +0100

    Added brackets to logging macros
    to avoid problems with bracketless conditions;

--- include/common.hpp
+++ include/common.hpp
@@ -7,18 +7,33 @@
 /***********************Macros**********************/
 //thread-safe logging macros
 #define VVLOG(format, ...)\
- VVLogMtx.lock(); fprintf (stderr, format, ## __VA_ARGS__); VVLogMtx.unlock()
+{\
+  VVLogMtx.lock();\
+  fprintf (stderr, format, ## __VA_ARGS__);\
+  VVLogMtx.unlock();\
+}
 
 #define VVLOG_SOURCE_INFO()\
- VVLogMtx.lock(); fprintf(stderr, "%s:%d In function %s\n", __FILE__, __LINE__,\
-__func__); VVLogMtx.unlock()
+{\
+  VVLogMtx.lock();\
+  fprintf(stderr, "%s:%d In function %s\n", __FILE__, __LINE__,__func__);\
+  VVLogMtx.unlock();\
+}
 
 #define VV_PRINT_MAT(mat)\
- VVLogMtx.lock(); printf("%s = \n", #mat); displayMatrix(mat); VVLogMtx.unlock()
+{\
+ VVLogMtx.lock(); printf("%s = \n", #mat);\
+  displayMatrix(mat);\
+  VVLogMtx.unlock();\
+}
 
 #define VV_LOG_RESULT_CODE(code)\
-   VVLogMtx.lock(); fprintf(stderr, "%s:%d %s=%s\n", __FILE__, __LINE__, #code,\
-                            resultCodeToString(code)); VVLogMtx.unlock()
+{\
+  VVLogMtx.lock();\
+  fprintf(stderr, "%s:%d %s=%s\n", __FILE__, __LINE__, #code,\
+          resultCodeToString(code));\
+   VVLogMtx.unlock();\
+}
 
 //common opencv numeric calculation data type
 #define numericDataType CV_64FC1
